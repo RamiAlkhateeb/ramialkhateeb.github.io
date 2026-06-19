9c3ad7a346b42c01269823ffbf5906059ecce256
RamiAlkhateeb <RamiAlkhateeb@users.noreply.github.com>
2026-06-19 05:45:42 +0000

    deploy: fd83a782e113ad41ed7096491e17c4cb358a6239

---
+++ src/AdminApp/Program.cs
@@ -0,0 +1,14 @@
+using AdminApp;
+using AdminApp.Services;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<GitHubService>(); // <-- Register your GitHub service component
+
+
+await builder.Build().RunAsync();
